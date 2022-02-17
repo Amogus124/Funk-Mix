98f06dbfc308ce9512b6b3554a2b1f421a1c12af
Amogus124 <95672691+Amogus124@users.noreply.github.com>
2022-02-17 10:17:59 -0300

    Add files via upload

---
+++ assets/preload/stages/mario.lua
@@ -0,0 +1,19 @@
+function onCreate()
+	-- background shit
+	makeLuaSprite("bg", "overworld/bg", 316, 386);
+	addLuaSprite("bg", false);
+	close(true)
+
+end
+
+function onBeatHit()
+
+end
+
+function onStepHit()
+
+end
+
+function onUpdate()
+
+end
---
+++ assets/preload/stages/mario2.lua
@@ -0,0 +1,19 @@
+function onCreate()
+	-- background shit
+	makeLuaSprite("bg", "underground/bg", 236, 346);
+	addLuaSprite("bg", false);
+	close(true)
+
+end
+
+function onBeatHit()
+
+end
+
+function onStepHit()
+
+end
+
+function onUpdate()
+
+end
